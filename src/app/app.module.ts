66788fa83930e102810a7f7dde81bde813722747
Israel Garcia <israel.garcia@decsef.com>
2020-10-21 21:25:10 -0500

    Subiendo proyecto para poder iniciar el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { HttpClientModule } from "@angular/common/http";
 
+import { InMemoryDataService } from "./in-memory-data.service";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      put204: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
